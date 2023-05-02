212e476ca323f8165d57a7a67c23082d06e57513
firstpj <114886262+firstpj@users.noreply.github.com>
2023-05-02 09:31:06 +0100

    modified files

--- script.js
+++ script.js
@@ -1,65 +1,62 @@
-const container = document.querySelector('.container')
-const titleInput = document.querySelector('#title')
-const authorInput = document.querySelector('#author')
-const submit = document.querySelector('#add-button')
-const form = document.querySelector('.form')
-function Books(title,author){
-  this.title = title
-  this.author = author
+const container = document.querySelector('.container');
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const submit = document.querySelector('#add-button');
+const form = document.querySelector('.form');
+function Books(title, author) {
+  this.title = title;
+  this.author = author;
 }
-let cards = []
+let cards = [];
 
 function addBook(title, author) {
   const book = new Books(title, author);
   cards.push(book);
   ShowCards();
   const dataMarker = JSON.stringify(cards);
-    localStorage.setItem('data', dataMarker);
+  localStorage.setItem('data', dataMarker);
 }
 window.addEventListener('DOMContentLoaded', (event) => {
   if (localStorage.getItem('data')) {
     cards = JSON.parse(localStorage.getItem('data'));
   }
   ShowCards();
+});
 
-})
-
-function ShowCards(){
-  const removeDivs = document.querySelectorAll('.card')
+function ShowCards() {
+  const removeDivs = document.querySelectorAll('.card');
   for (let i = 0; i < removeDivs.length; i++) {
-    removeDivs[i].remove()
+    removeDivs[i].remove();
   }
-  cards.map((card)=>{
-    const div = document.createElement('div')
-    div.classList.add('card')
-    const heading = document.createElement('h1')
-    const paragraph = document.createElement('p')
-    const deleteBook = document.createElement('button')
-    deleteBook.classList.add('delete-book')
-    heading.textContent = `${card.title}`
-    paragraph.textContent = `${card.author}`
-    deleteBook.textContent = 'Remove'
-    div.appendChild(heading)
-    div.appendChild(paragraph)
-    div.appendChild(deleteBook)
-    container.appendChild(div)
-  
-  deleteBook.addEventListener('click', (e) => {
-     cards.splice(cards.indexOf(card), 1);
-     e.target.parentNode.remove()
-     console.log(cards)
-     const dataMarker = JSON.stringify(cards);
-     localStorage.setItem('data', dataMarker);
+  cards.map((card) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    const heading = document.createElement('h1');
+    const paragraph = document.createElement('p');
+    const deleteBook = document.createElement('button');
+    deleteBook.classList.add('delete-book');
+    heading.textContent = `${card.title}`;
+    paragraph.textContent = `${card.author}`;
+    deleteBook.textContent = 'Remove';
+    div.appendChild(heading);
+    div.appendChild(paragraph);
+    div.appendChild(deleteBook);
+    container.appendChild(div);
 
+    deleteBook.addEventListener('click', (e) => {
+      cards.splice(cards.indexOf(card), 1);
+      e.target.parentNode.remove();
+      console.log(cards);
+      const dataMarker = JSON.stringify(cards);
+      localStorage.setItem('data', dataMarker);
+      
+    });
   });
-  })  
 }
-submit.addEventListener('click',()=>{
-  if(titleInput.value == ''&&authorInput.value == ""){
-    return;
-  }else{
-    addBook(titleInput.value,authorInput.value)
-    form.reset()
-    console.log(cards)
+submit.addEventListener('click', () => {
+  if (titleInput.value === '' && authorInput.value === '') {
+    return null;
   }
-})
+  addBook(titleInput.value, authorInput.value);
+  form.reset();
+});
